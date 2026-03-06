50c75ed2f9b0b4cd700216edacbe47ce88196887
1111 <ghugarkarsatyam1@gmail.com>
2026-03-06 23:22:10 +0530

    Add files via upload

---
+++ outliers/a.py
@@ -0,0 +1,25 @@
+
+
+def aa(i):
+
+    a=2+i
+
+    return a
+
+
+# b=aa(3)
+# print(b)
+
+# def clone_repo(repo_url):
+#     """Clone public Git repo to unique temp dir"""
+#     local_path = get_temp_path()
+
+#     try:
+#         print(f"Cloning to: {local_path}")
+#         repo = git.Repo.clone_from(repo_url, local_path, depth=1)  # Shallow clone = faster
+#         print("Clone successful!")
+#         return local_path
+    
+#     repo_url='dmmdcmkmk//fef/eg//'
+    
+#     local_path = clone_repo(repo_url)
---
+++ outliers/b.py
@@ -0,0 +1,4 @@
+from a import aa
+
+b=aa(3)
+print(b)
